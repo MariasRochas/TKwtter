dd0bac7c76ebed21bf7420740b8218e092dfd771
MariasRochas <132393830+MariasRochas@users.noreply.github.com>
2023-09-12 15:43:10 -0300

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,16 @@
+firebase.initializeApp(firebaseConfig);
+
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+    firebase.database().ref("/").child(user_name).update({
+        purpose : "adding user"
+    });
+    
+    localStorage.setItem("user_name", user_name);
+
+    window.locatio = "chat_room.html";
+}
+
+
+
